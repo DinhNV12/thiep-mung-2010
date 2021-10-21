099410bf66f9085d879b83ee0cab4fe92a5927dd
DinhNV12 <90915470+DinhNV12@users.noreply.github.com>
2021-10-21 19:09:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Cậu :>';
+const contentCard = 'Chúc cậu 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau Wish you all the best <3 ';
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
